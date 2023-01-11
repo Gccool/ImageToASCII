fe9dcf4e0855af866dbc8cf6f583829cd8573cde
Gccool <97748617+Gccool@users.noreply.github.com>
2023-01-11 18:04:00 +0000

    Add files via upload

---
+++ Png to ASCII_Color.py
@@ -0,0 +1,80 @@
+import math
+import cv2
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+import time
+import colorsys
+
+startTime = time.time()
+
+Path = r'C:\Users\georg\Pictures\Goat.jpeg'
+Outputfile = open(r"C:\Users\georg\Pictures\ASCII.txt", "w")
+img = cv2.imread(Path)
+
+characters = [' ', '.', '_', ':', ';', '#', '@']
+
+
+Answer = ''
+
+Margin = Offset = 40
+
+DeScaleAmount = 1
+ShouldDownScale = False
+
+if ShouldDownScale:
+    Img = Image.new('RGB', (int((img.shape[0] * 15) / DeScaleAmount), int((img.shape[1] * 15) / DeScaleAmount)), (25, 25, 25))
+if ShouldDownScale == False:
+    Img = Image.new('RGB', (img.shape[0] * 15, int(img.shape[1] * 15)), (25, 25, 25))
+d = ImageDraw.Draw(Img)
+
+
+
+i = 0
+
+
+if ShouldDownScale:
+    for y in range(0, int(img.shape[0] / DeScaleAmount)):
+        for x in range(0, int(img.shape[1] / DeScaleAmount)):
+            b,g,r = (img[y * DeScaleAmount, x * DeScaleAmount])
+            luminance = (r * 0.3) + (g * 0.59) + (b * 0.11)
+            Char = characters[int(luminance / 40)]
+            Answer += Char
+            Answer += ' '
+            d.text((int((x * 10)) + 5, int((y * 10))), Char, fill=(r, g, b, 255)) #FIX TEXT SCALLING
+            #d.text((x, y), Answer, fill=(r, g, b))
+            print(i)
+            i += 1
+        Answer += '\n'
+        
+if ShouldDownScale == False:
+    for y in range(0, img.shape[0]):
+        for x in range(0, img.shape[1]):
+            b,g,r = (img[y, x])
+            luminance = (r * 0.3) + (g * 0.59) + (b * 0.11)
+            Char = characters[int(luminance / 40)]
+            Answer += Char
+            Answer += ' '
+            d.text((int(x * 10) + 5, int(y * 10)), Char, fill=(r, g, b, 255)) #FIX TEXT SCALLING
+            #d.text((x, y), Answer, fill=(r, g, b))
+            print(i)
+            i += 1
+            
+            
+        Answer += '\n'
+
+#img = Image.new('RGB', (img.shape[0] * 18, int(img.shape[1] * 10.1)), (0, 0, 0))
+#d = ImageDraw.Draw(img)
+#d.text((5, 5), Answer, fill=(255, 255, 255))
+Img.save(r"C:\Users\georg\Pictures\ASCIIImg_Color.png")
+
+a = Outputfile.write(Answer)        
+Outputfile.close()
+print(a)  
+
+EndTime = time.time()
+print(EndTime - startTime)
+
+
+time.sleep(999999)
+        
---
+++ Png to ASCII_GrayScale.py
@@ -0,0 +1,69 @@
+import math
+import cv2
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+import time
+
+startTime = time.time()
+Path = r'C:\Users\georg\Pictures\Goat.jpeg'
+Outputfile = open(r"C:\Users\georg\Pictures\ASCII.txt", "w")
+img = cv2.imread(Path)
+
+characters = [' ', '.', '_', ':', ';', '#', '@']
+
+Answer = ''
+
+DeScaleAmount = 1
+ShouldDeScale = True
+#Img = Image.new('RGB', (img.shape[0] * 18, int(img.shape[1] * 10.1)), (0, 0, 0))
+#d = ImageDraw.Draw(Img)
+
+
+i = 0
+
+
+if ShouldDeScale:
+    for y in range(0, int(img.shape[0] / DeScaleAmount)):
+        for x in range(0, int(img.shape[1] / DeScaleAmount)):
+            b,g,r = (img[y * DeScaleAmount, x * DeScaleAmount])
+            luminance = (r * 0.3) + (g * 0.59) + (b * 0.11)
+            Char = characters[int(luminance / 40)]
+            Answer += Char
+            Answer += ' '
+            #d.text((x, y), Answer, fill=(r, g, b))
+            print(i)
+            i += 1
+        Answer += '\n'
+        
+if ShouldDeScale == False:
+    for y in range(0, img.shape[0]):
+        for x in range(0, img.shape[1]):
+            b,g,r = (img[y, x])
+            luminance = (r * 0.3) + (g * 0.59) + (b * 0.11)
+            Char = characters[int(luminance / 40)]
+            Answer += Char
+            Answer += ' '
+            #d.text((x, y), Answer, fill=(r, g, b))
+            print(i)
+            i += 1
+        Answer += '\n'
+
+if ShouldDeScale:
+    Img = Image.new('RGB', (int((img.shape[0] * 18) / DeScaleAmount), int((img.shape[1] * 15.5) / DeScaleAmount)), (0, 0, 0))
+if ShouldDeScale == False:
+    Img = Image.new('RGB', (int((img.shape[0] * 18)), int((img.shape[1] * 15.5))), (0, 0, 0))
+d = ImageDraw.Draw(Img)
+d.text((5, 5), Answer, fill=(255, 255, 255))
+Img.save(r"C:\Users\georg\Pictures\ASCIIImg_GrayScale.png")
+
+a = Outputfile.write(Answer)        
+Outputfile.close()
+print(a)  
+
+EndTime = time.time()
+
+while True:
+    print(EndTime - startTime)
+
+        
